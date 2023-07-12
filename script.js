e9faebc6f71b8dd8939ebe3a4cb98028a3d303ff
TheOnlyROB <brkeighd@gmail.com>
2023-07-12 15:41:32 +0200

    nerfed eight ball img, new java, css & html changs

---
+++ script.js
@@ -0,0 +1,31 @@
+const answers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later.",
+    "Better not tell you now",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ];
+  
+  function getAnswers() {
+    const index = Math.floor(Math.random() * answers.length);
+    const answer = answers[index];
+  
+    const answerDisplay = document.getElementById("answer-display");
+    answerDisplay.textContent = answer;
+  }
+  
